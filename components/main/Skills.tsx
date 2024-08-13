ebe532a66331b621bc5458c2f940e086e9957114
Syed Yawar Ali Turab <77963884+aliturab99@users.noreply.github.com>
2024-08-13 15:08:32 +0500

    Nit

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -6,8 +6,6 @@ import {
   Backend_skill,
   Frontend_skill,
   Full_stack,
-  Other_skill,
-  Skill_data,
 } from "@/constants";
 import SkillDataProvider from "../sub/SkillDataProvider";
 import SkillText from "../sub/SkillText";
